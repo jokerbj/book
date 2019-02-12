f6b4d72e35ff52aa5a55c2691345b0dc92b875f8
jokerbj <jokerbj@126.com>
2019-02-12 20:39:31 +0800

    11111

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git config user.name "jokerbj"
+git config user.email "849185108@qq.com"
+cd _book
+git add .
+git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+git remote add origin git@github.com:jokerbj/book.git
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
